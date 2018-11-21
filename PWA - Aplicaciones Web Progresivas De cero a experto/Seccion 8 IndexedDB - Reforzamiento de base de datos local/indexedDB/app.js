fed1ae20b09cfabf82c859f1807b0e1ced99bcc0
Ander Cadaval <ander.cadaval@gmail.com>
2018-11-21 21:14:37 +0100

    master : PWA Sección IndexedDB

---
+++ PWA - Aplicaciones Web Progresivas De cero a experto/Seccion 8 IndexedDB - Reforzamiento de base de datos local/indexedDB/app.js
@@ -0,0 +1,50 @@
+if (window.indexedDB) {
+    // indexedDB: Reforzamiento
+    let request = window.indexedDB.open('mi-database', 1);
+
+    //Se actualiza cuando se crea o se sube de versión de la base de datos
+    request.onupgradeneeded = event => {
+        console.log('Actualización de BD');
+
+        let db = event.target.result;
+
+        db.createObjectStore('heroes', {
+            keyPath: 'id'
+        });
+    };
+
+    //Manejo de errores
+    request.onerror = event => {
+        console.log('DB error:', event.target.error);
+    };
+
+    //Insertar datos
+    request.onsuccess = event => {
+        let db = event.target.result;
+        let heroesData = [
+            { id: '1111', heroe: 'Spiderman', mensaje: 'Aqui su amigo Spiderman' },
+            { id: '2222', heroe: 'Ironman', mensaje: 'Aqui en mi nuevo Mark 50' }
+        ];
+
+        let heroesTransaction = db.transaction('heroes', 'readwrite');
+
+        heroesTransaction.onerror = event => {
+            console.log('Error guardando:', event.target.error);
+        };
+
+        //Informa sobre el éxito de la transacción
+        heroesTransaction.oncomplete = event => {
+            console.log('Transaccion hecha', event);
+        };
+
+        let heroesStore = heroesTransaction.objectStore('heroes');
+
+        for (let heroe of heroesData) {
+            heroesStore.add(heroe);
+        }
+
+        heroesStore.onsuccess = event => {
+            console.log('Nuevo item agregado a la base de datos');
+        };
+    };
+}
---
+++ PWA - Aplicaciones Web Progresivas De cero a experto/Seccion 8 IndexedDB - Reforzamiento de base de datos local/tarea-pouchdb-manual/app.js
@@ -0,0 +1,65 @@
+// Entrenamiento PouchDB
+
+// 1- Crear la base de datos
+// Nombre:  mensajes
+const db = new PouchDB('mensajes');
+
+
+// Objeto a grabar en base de datos
+const mensaje = {
+    _id: new Date().toISOString(),
+    user: 'spiderman',
+    mensaje: 'Mi tía hizo unos panqueques muy buenos',
+    sincronizado: false
+};
+
+
+// 2- Insertar en la base de datos
+db.put(mensaje)
+    .then(console.log('Registro Insertado'));
+
+
+// 3- Leer todos los mensajes offline
+//que aparezcan en la consola
+
+db.allDocs({ include_docs: true, descending: true }).then(docs => {
+    console.log(docs.rows);
+
+    docs.rows.forEach(mensaje => console.log(mensaje.doc));
+});
+
+
+
+
+// 4- Cambiar el valor 'sincronizado' de todos los objetos
+//  en la BD a TRUE
+
+db.allDocs({ include_docs: true, descending: true }).then(docs => {
+    console.log(docs.rows);
+
+    docs.rows.forEach(mensaje => {
+        console.log(mensaje.doc);
+        mensaje.doc.sincronizado = true;
+
+        db.put(mensaje.doc).then(console.log('Registro actualizado'));
+        console.log(mensaje.doc);
+    });
+});
+
+
+
+// 5- Borrar todos los registros, uno por uno, evaluando
+// cuales estan sincronizados
+// deberá de comentar todo el código que actualiza
+// el campo de la sincronización
+
+db.allDocs({ include_docs: true, descending: true }).then(docs => {
+    console.log(docs.rows);
+
+    docs.rows.forEach(mensaje => {
+        console.log(mensaje.doc);
+        if (mensaje.doc.sincronizado) {
+            db.remove(mensaje.doc).then(console.log('Registro eliminado'));
+        }
+    });
+});
