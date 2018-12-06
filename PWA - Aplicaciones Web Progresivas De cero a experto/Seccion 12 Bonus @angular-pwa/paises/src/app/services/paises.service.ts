0cb3fe4f806b3781f3ab048dc00f118df8b2d665
Ander Cadaval <ander.cadaval@gmail.com>
2018-12-06 20:17:17 +0100

    master. PWA Sección Angular

---
+++ PWA - Aplicaciones Web Progresivas De cero a experto/Seccion 12 Bonus @angular-pwa/paises/src/app/services/paises.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaisInterface } from '../interfaces/pais.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PaisesService {
+
+    private paises: PaisInterface[] = [];
+
+    constructor(private http: HttpClient) { }
+
+    getPaises(): Promise<PaisInterface[]> {
+        if (this.paises.length !== 0) {
+            return Promise.resolve(this.paises);
+        }
+
+        return new Promise(resolve => {
+            this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe((paises: PaisInterface[]) => {
+                console.log(paises);
+                this.paises = paises;
+                resolve(paises);
+            });
+        });
+    }
+
+    getPais(id: string): Promise<PaisInterface> {
+        if (this.paises.length !== 0) {
+            // Hay paises cargados
+            const pais = this.paises.find(p => p.alpha3Code === id);
+
+            return Promise.resolve(pais);
+        }
+
+        return this.getPaises().then(paises => {
+            const pais = this.paises.find(p => p.alpha3Code === id);
+
+            return Promise.resolve(pais);
+        });
+    }
+}
